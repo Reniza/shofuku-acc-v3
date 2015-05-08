9f77bc64fd24f676842febcae3485a9756c753d4
Reniza <azhee_18@yahoo.com>
2015-05-09 00:28:39 +0800

    login function running

--- src/com/shofuku/accsystem/action/LoginAction.java
+++ src/com/shofuku/accsystem/action/LoginAction.java
@@ -1,86 +1,45 @@
 package com.shofuku.accsystem.action;
 import com.opensymphony.xwork2.ActionSupport;
+import com.shofuku.accsystem.domain.security.UserAccount;
+import com.shofuku.accsystem.utils.HibernateUtil;
+import com.shofuku.accsystem.controllers.SecurityManager;
+
 import java.util.Date;
+
+import org.hibernate.Session;
 
 
 public class LoginAction extends ActionSupport {
 /**
 	 * <p> Validate a user login. </p>
 	 */
-		 
+	SecurityManager manager = new SecurityManager();
+	private String username;
+	private String password;
+	
+	private Session getSession() {
+		return HibernateUtil.getSessionFactory().getCurrentSession();
+	}
+	
 	public String execute() throws Exception {
-	  
+		Session session = getSession();
+		UserAccount user = (UserAccount) manager.listSecurityByParameter(UserAccount.class, "userName", this.getUsername(), session).get(0);
+		
 		if (getUsername().length() == 0 || getPassword().length() ==0 ){
 			validate(); 
-			
 		}
-		else if(!getUsername().equals("Admin") || !getPassword().equals("Admin")){
+		else if(!getUsername().equals(user.getUserName().trim()) || !getPassword().equals(user.getPassword().trim())){
 			addActionError("Invalid user name or password! Please try again!");
-	  
 			return ERROR;
 		}
-	  if(getUsername().equals("Admin") && getPassword().equals("Admin")){
-		  return SUCCESS;
-	  }else{
-		  return NONE;
-	  }
+		
+		if(getUsername().equals(user.getUserName()) && getPassword().equals(user.getPassword())){
+			  return SUCCESS;
+		  }else{
+			  return NONE;
+		  }
 	}
 
-
-	  // ---- Username property ----
-
-	  /**
-	 * <p>Field to store User username.</p>
-	 * <p/>
-	 */
-	  private String username = null;
-
-
-	  /**
-	 * <p>Provide User username.</p>
-	 *
-	 * @return Returns the User username.
-	 */
-	  public String getUsername() {
-	  return username;
-	  }
-
-	  /**
-	 * <p>Store new User username</p>
-	 *
-	 * @param value The username to set.
-	 */
-	  public void setUsername(String value) {
-	  username = value;
-	  }
-
-	  // ---- Username property ----
-
-	  /**
-	 * <p>Field to store User password.</p>
-	 * <p/>
-	 */
-	  private String password = null;
-
-
-	  /**
-	 * <p>Provide User password.</p>
-	 *
-	 * @return Returns the User password.
-	 */
-	  public String getPassword() {
-	  return password;
-	  }
-
-	  /**
-	 * <p>Store new User password</p>
-	 *
-	 * @param value The password to set.
-	 */
-	  public void setPassword(String value) {
-	  password = value;
-	  }
-	  
 	  @Override
 		public void validate() {
 			if (getUsername().length() == 0){
@@ -92,6 +51,22 @@ public class LoginAction extends ActionSupport {
 			}
 		}
 
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	  
 }
 
 
